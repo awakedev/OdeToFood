eed66a5e4614de444fad36cc3b13e5ecdaa61754
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-04-06 20:46:11 +0100

    tests

---
+++ OdeToFood/OdeToFood/Pages/Restauraunts/Detail.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+
+namespace OdeToFood.Pages.Restauraunts
+{
+    public class DetailModel : PageModel
+    {
+        public Restauraunt Restauraunt { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
